876b6d7d0d24a88509b68c0812f802cd29be76c3
EvieePy <29671945+EvieePy@users.noreply.github.com>
2025-09-26 12:20:33 +1000

    Implement warn and timeout and add docstrings.

--- extensions/mods.py
+++ extensions/mods.py
@@ -25,15 +25,42 @@ class ModeratorComponent(commands.Component):
         self._raid: twitchio.User | None = None
 
     @core.permissions_check(perms=core.ModPermissions.timeout)
-    async def timeout(self) -> None: ...
+    async def timeout(self, ctx: commands.Context[core.Bot], user: twitchio.User, duration: int, *, reason: str) -> None:
+        """Timeout a user for a specified amount of time (as seconds).
+        Usage: !timeout <user> <duration> [reason]. E.g. !timeout mystypy 600 for being a loser.
+        """
+        try:
+            await ctx.broadcaster.timeout_user(moderator=self.bot.user, user=user, duration=duration, reason=reason)
+        except twitchio.HTTPException as e:
+            await ctx.reply(f"An error occurred attempting to timeout {user}: {e}")
+            return
+
+        await ctx.reply("VoteYea")
 
     @core.permissions_check(perms=core.ModPermissions.warn)
-    async def warn(self) -> None: ...
+    async def warn(self, ctx: commands.Context[core.Bot], user: twitchio.User, *, reason: str) -> None:
+        """Warn a user with the provided reason.
+        Usage: !warn <user> <reason>. E.g. !warn mystypy for being a loser.
+        """
+        assert self.bot.user
+
+        try:
+            await ctx.broadcaster.warn_user(moderator=self.bot.user, user_id=user, reason=reason)
+        except twitchio.HTTPException as e:
+            await ctx.reply(f"An error occurred attempting to warn {user}: {e}")
+            return
+
+        await ctx.reply("VoteYea")
 
     @core.permissions_check(perms=core.ModPermissions.shoutout)
     @commands.command(aliases=["so", "shout"])
     @commands.cooldown(rate=2, per=120, key=commands.BucketType.channel)
     async def shoutout(self, ctx: commands.Context[core.Bot], *, user: twitchio.User) -> None:
+        """Send a shoutout announcement to a user.
+        Usage: !shoutout <user>. E.g. !shoutout mystypy
+
+        This command has a cooldown of 2/120s.
+        """
         info = await user.fetch_channel_info()
         url = f"https://twitch.tv/{user.name}"
 
@@ -44,23 +71,37 @@ class ModeratorComponent(commands.Component):
     @core.permissions_check(perms=core.ModPermissions.raid)
     @commands.group(invoke_fallback=True)
     async def raid(self, ctx: commands.Context[core.Bot], *, user: twitchio.User) -> None:
+        """Start a raid to the provided channel.
+        Usage: !raid <user> E.g. !raid mystypy
+        """
         if self._raid:
             await ctx.reply("A raid has already begun!")
             return
 
-        await ctx.broadcaster.start_raid(to_broadcaster=user.id)
-        self._raid = user
+        try:
+            await ctx.broadcaster.start_raid(to_broadcaster=user.id)
+        except twitchio.HTTPException as e:
+            await ctx.reply(f"Starting a raid to {user} failed: {e}")
+            return
 
+        self._raid = user
         await ctx.send(f"{ctx.chatter.mention} has started a raid to {user.mention}!")
 
     @core.permissions_check(perms=core.ModPermissions.raid)
     @raid.command(name="cancel")
     async def raid_cancel(self, ctx: commands.Context[core.Bot]) -> None:
+        """Cancel an ongoing raid started by a moderator.
+        Usage: !raid cancel
+        """
         if not self._raid:
             await ctx.reply("There is no current raid in progress.")
             return
 
-        await ctx.broadcaster.cancel_raid()
+        try:
+            await ctx.broadcaster.cancel_raid()
+        except twitchio.HTTPException as e:
+            await ctx.reply(f"Unable to cancel raid: {e}")
+            return
 
         user = self._raid
         self._raid = None
@@ -72,6 +113,9 @@ class ModeratorComponent(commands.Component):
 
     @commands.command()
     async def perms(self, ctx: commands.Context[core.Bot], *, user: twitchio.User | None = None) -> None:
+        """Check your fine-grained moderator permissions.
+        Usage: !perms
+        """
         chatter = user or ctx.chatter
         payload = await self.bot.db.fetch_mod(chatter.id)
 
